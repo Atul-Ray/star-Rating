1da37833d43504b4a314fb84a91cc2c4df138a52
Atul <atulray371@gmail.com>
2024-10-28 00:58:04 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from "react";
-import Accordion from "./Compoent/accordion";
+import ColorGenerator from "./Compoent/colorGenerator";
 
 function App() {
   return (
     <>
-     
-      <div className="w-screen h-screen flex justify-center items-center  ">
-      <div className="w-3/4 h-4/5 max-[600px]:w-full  rounded-lg overflow-hidden  bg-slate-950 ">  
-
-      <Accordion />
-      </div>
-      </div>
+     <div className="h-screen w-screen bg-slate-300 flex justify-center items-center">
+       <ColorGenerator/>
+     </div>
+      
     </>
   );
 }
